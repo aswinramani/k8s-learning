efb3141bdc65da0ca5e6fd53a3974230ff0161b5
Aswin Ramani <aswinramani5@gmail.com>
2026-08-15 23:34:19 +0530

    day2: add rbac learning at namespace level

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # k8s-learning
 
 Day 1: Learnt about replicaSet type using nginx-rs yaml example. 
+Day 2: Learnt how to set role, service account, bind role to service account using rolebinding, added script to generate yaml file and validated by running apply -f with dry run mode. Yaml is valid. 
+
+Service account => Permissions that apps like argo, prometheus, flux require to do their job. 
+
+kubectl create serviceaccount <serviceaccountname> --namespace=<namespacename> --dry-run=client -o yaml >> <dirfilepathyamlfilename>
+
+kubectl create => is imperative 
+kubectl apply -f => is declarative
---
+++ scripts/rbac-demo-init.sh
@@ -0,0 +1,18 @@
+BASE_DIR="$(dirname "$0")/.."
+MANIFESTS_DIR="$BASE_DIR/manifests"
+RBAC_DEMO_MANIFEST_FILE="$MANIFESTS_DIR/rbac-demo.yaml"
+kubectl create namespace rbac-demo --dry-run=client -o yaml > "$RBAC_DEMO_MANIFEST_FILE"
+echo "---" >> "$RBAC_DEMO_MANIFEST_FILE"
+kubectl create serviceaccount pod-reader-app -n rbac-demo --dry-run=client -o yaml >> "$RBAC_DEMO_MANIFEST_FILE"
+echo "---" >> "$RBAC_DEMO_MANIFEST_FILE"
+kubectl create role pod-reader \
+  --verb=get,list,watch \
+  --resource=pods \
+  --namespace=rbac-demo \
+  --dry-run=client -o yaml >> "$RBAC_DEMO_MANIFEST_FILE"
+echo "---" >> "$RBAC_DEMO_MANIFEST_FILE"
+kubectl create rolebinding pod-reader-binding \
+  --role=pod-reader \
+  --serviceaccount=rbac-demo:pod-reader-app \
+  --namespace=rbac-demo \
+  --dry-run=client -o yaml >> "$RBAC_DEMO_MANIFEST_FILE"
